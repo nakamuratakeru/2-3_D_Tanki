ac921286fd544991637a1d45788fc089662284d1
KBC12005020\kbc12005020 <junichiro-inafuku-20f@stu.kbc.ac.jp>
2022-01-27 09:35:40 +0900

    試し、エネミー追いかけるスクリプト

---
+++ 2-3D/Assets/Script/EnemyZone.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyZone : MonoBehaviour
+{
+
+    public int EnemyArea = 10;
+    int gamecount = 0;
+    [SerializeField] private GameObject GameOverText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gamecount = 1;
+            Time.timeScale = 0;
+        }
+    }
+
+
+    //public void SetTarget(Transform target)
+    //{
+    //    this.target = target;
+    //}
+
+    //    private void OnTriggerStay(Collider col)
+    //{
+    //    if (col.gameObject.tag == "Player")
+    //    {
+
+    //        GameOverText.SetActive(true);
+    //        Time.timeScale = 0;
+
+
+
+    //    }
+    //}
+
+
+}
