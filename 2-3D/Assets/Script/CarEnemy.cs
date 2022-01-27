faaf52f6c4e02fffd1104fd9e247b928efade1ab
KBC12005020\kbc12005020 <junichiro-inafuku-20f@stu.kbc.ac.jp>
2022-01-27 15:38:33 +0900

    コミット処理

--- 2-3D/Assets/Script/CarEnemy.cs
+++ 2-3D/Assets/Script/CarEnemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class carenemy : MonoBehaviour
+public class CarEnemy : MonoBehaviour
 {
     CharacterController Controller;
     Transform Target;
---
+++ 2-3D/Assets/Script/EnemyZone1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EnemyZone1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("owari");
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
